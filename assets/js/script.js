d74375a5a478bbbc7b32e0caf59bf89247c679eb
Byron Roark <byron@byronroark.com>
2020-02-12 23:57:35 -0500

    fix spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,14 +40,14 @@ calculate();
 $('div').delegate('input:checkbox', 'click', calculate);
 
 function submitForm() {
-    var response = grecaptcha.getResponse();
-    if(response.length == 0) {
-        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required</span>';
-        return false;
-    }
-    return true;
+  var response = grecaptcha.getResponse();
+  if(response.length == 0) {
+    document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required</span>';
+    return false;
+  }
+  return true;
 }
  
 function verifyCaptcha() {
-    document.getElementById('g-recaptcha-error').innerHTML = '';
+  document.getElementById('g-recaptcha-error').innerHTML = '';
 }
