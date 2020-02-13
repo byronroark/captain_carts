98b7c9e10417ab723650c4ea83da1d88c72250a6
Byron Roark <byron@byronroark.com>
2020-02-12 23:51:04 -0500

    revert captcha

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,16 +38,3 @@ function calculate() {
 
 calculate();
 $('div').delegate('input:checkbox', 'click', calculate);
-
-function submitForm() {
-    var response = grecaptcha.getResponse();
-    if(response.length == 0) {
-        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required</span>';
-        return false;
-    }
-    return true;
-}
- 
-function verifyCaptcha() {
-    document.getElementById('g-recaptcha-error').innerHTML = '';
-}
